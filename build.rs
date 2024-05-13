003e740381629d334d9e87fe9257afd04f15e3c2
Michael Winkelmann <michael@winkelmann.site>
2024-05-13 17:43:07 +0200

    Build with Slint

--- build.rs
+++ build.rs
@@ -1,3 +1,9 @@
 fn main() {
     embuild::espidf::sysenv::output();
+    slint_build::compile_with_config(
+        "ui/appwindow.slint",
+        slint_build::CompilerConfiguration::new()
+            .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer),
+    )
+    .unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+slint::include_modules!();
+
+
+fn create_slint_app() -> AppWindow {
+    let ui = AppWindow::new().expect("Failed to load UI");
+
+    let ui_handle = ui.as_weak();
+    ui.on_request_increase_value(move || {
+        let ui = ui_handle.unwrap();
+        ui.set_counter(ui.get_counter() + 1);
+    });
+    ui
+}
+
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -7,4 +22,6 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+    create_slint_app().run().unwrap();
 }
