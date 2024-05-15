2070abd62469262f296676708d27f4d71731b136
Michael Winkelmann <michael@winkelmann.site>
2024-05-15 16:22:51 +0200

    impl Platform for EspPlatform
    based on C++ impl from slint-esp.cpp

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,167 @@
 extern crate alloc;
 
+use esp_idf_svc::hal::sys::*;
+
+
+const DISPLAY_WIDTH: usize = 320;
+const DISPLAY_HEIGHT: usize = 240;
+const DRAW_BUFFER_SIZE: usize = DISPLAY_WIDTH * DISPLAY_HEIGHT; // @todo find constants
+
+struct EspPlatform {
+    size: slint::PhysicalSize,
+    panel_handle: esp_lcd_panel_handle_t,
+    touch_handle: Option<*mut esp_lcd_touch_s>,
+    window: Rc<MinimalSoftwareWindow>,
+}
+
+impl Platform for EspPlatform {
+    fn create_window_adapter(
+        &self,
+    ) -> Result<Rc<dyn slint::platform::WindowAdapter>, slint::PlatformError> {
+        // Since on MCUs, there can be only one window, just return a clone of self.window.
+        // We'll also use the same window in the event loop.
+        Ok(self.window.clone())
+    }
+    fn duration_since_start(&self) -> core::time::Duration {
+        unsafe {
+            let ticks = xTaskGetTickCount(); // @fixme Are ticks in microseconds?
+            core::time::Duration::from_micros(ticks as u64)
+        }
+    }
+    // optional: You can put the event loop there, or in the main function, see later
+    fn run_event_loop(&self) -> Result<(), slint::PlatformError> {
+        unsafe {
+            //self.task = xTaskGetCurrentTaskHandle(); // @fixme How to set task handle?
+            esp_lcd_panel_disp_on_off(self.panel_handle, true);
+
+            let mut max_ticks_to_wait: u16 = 0xffff; // = portMAX_DELAY
+
+            if let Some(touch_handle) = self.touch_handle {
+                //@fixme: How to handle touch interrupts and where is vTaskNotifyGiveFromISR defined?
+                //if esp_lcd_touch_register_interrupt_callback(touch_handle, move || { vTaskNotifyGiveFromISR(self.task, core::ptr::null_mut()); }) != 0 {
+                //    return Err(slint::PlatformError::FailedToRegisterTouchInterrupt);
+                //}
+            }
+
+            let mut last_touch_x = 0;
+            let mut last_touch_y = 0;
+            let mut touch_down = false;
+
+            loop {
+                slint::platform::update_timers_and_animations();
+
+                if let Some(touch_handle) = self.touch_handle {
+                    let mut touchpad_x = [0];
+                    let mut touchpad_y = [0];
+                    let mut touchpad_cnt = [0 as u8];
+
+                    esp_lcd_touch_read_data(touch_handle);
+
+                    let touchpad_pressed = esp_lcd_touch_get_coordinates(
+                        touch_handle,
+                        touchpad_x.as_mut_ptr(),
+                        touchpad_y.as_mut_ptr(),
+                        std::ptr::null_mut(),
+                        touchpad_cnt.as_mut_ptr(),
+                        1,
+                    );
+
+                    if touchpad_pressed && touchpad_cnt[0] > 0 {
+                        last_touch_x = touchpad_x[0] as i32;
+                        last_touch_y = touchpad_y[0] as i32;
+
+                        self.window
+                            .dispatch_event(slint::platform::WindowEvent::PointerMoved {
+                                position: slint::LogicalPosition::new(
+                                    last_touch_x as f32,
+                                    last_touch_y as f32,
+                                ),
+                            });
+
+                        if !touch_down {
+                            touch_down = true;
+                            self.window.dispatch_event(
+                                slint::platform::WindowEvent::PointerPressed {
+                                    position: slint::LogicalPosition::new(
+                                        last_touch_x as f32,
+                                        last_touch_y as f32,
+                                    ),
+                                    button: slint::platform::PointerEventButton::Left,
+                                },
+                            );
+                        }
+
+                        touch_down = true;
+                    } else if touch_down {
+                        touch_down = false;
+                        self.window
+                            .dispatch_event(slint::platform::WindowEvent::PointerReleased {
+                                position: slint::LogicalPosition::new(
+                                    last_touch_x as f32,
+                                    last_touch_y as f32,
+                                ),
+                                button: slint::platform::PointerEventButton::Left,
+                            });
+                    }
+
+                    use slint::platform::software_renderer::Rgb565Pixel;
+                    let mut buffer = [Rgb565Pixel(0); DRAW_BUFFER_SIZE];
+
+
+                    // Draw the scene if something needs to be drawn.
+                    self.window.clone().draw_if_needed(move|renderer| {
+                        // Do the rendering!
+                        let region = renderer.render(&mut buffer, self.size.width as usize);
+
+                        for (o, s) in region.iter() {
+                            for y in o.y..(s.height as i32) {
+                                for x in o.x..(s.width as i32) {
+                                    let offset = (y * s.width as i32 + x) as usize;
+                                    let pixel = buffer[offset].0;
+                                    // Convert pixel to big endian
+                                    let pixel = ((pixel & 0xff) << 8) | ((pixel & 0xff00) >> 8);
+                                    buffer[offset].0 = pixel;
+                                }
+                                
+                                use std::ffi::c_void;
+                                esp_lcd_panel_draw_bitmap(
+                                    self.panel_handle,
+                                    o.x,
+                                    y,
+                                    o.x + s.width as i32,
+                                    y + 1,
+                                    buffer.as_ptr().wrapping_add((y * self.size.width as i32 + o.x) as usize).cast::<c_void>(),
+                                );
+                            }
+                        }
+                    });
+
+                    // Try to put the MCU to sleep
+                    if !self.window.has_active_animations() {
+                        continue;
+                    }
+                }
+
+/* 
+        TickType_t ticks_to_wait = max_ticks_to_wait;
+        if (auto wait_time = slint::platform::duration_until_next_timer_update()) {
+            ticks_to_wait = std::min(ticks_to_wait, pdMS_TO_TICKS(wait_time->count()));
+        }
+
+        ulTaskNotifyTake(/*reset to zero*/ pdTRUE, ticks_to_wait); */
+            }
+        }
+    }
+
+    //vTaskDelete(NULL);
+
+}
+
 use alloc::rc::Rc;
 
 slint::include_modules!();
 
-fn check_for_touch_event() -> Option<slint::platform::WindowEvent> {
-    todo!()
-}
-mod hal {
-    pub struct Timer();
-    impl Timer {
-        pub fn get_time(&self) -> u64 {
-            todo!()
-        }
-    }
-
-    pub fn wfi() {}
-}
-
 use slint::platform::{software_renderer::MinimalSoftwareWindow, Platform, WindowEvent};
-
-fn is_swap_pending() -> bool {
-    false
-}
-fn swap_buffers() {}
 
 fn create_slint_app() -> AppWindow {
     let ui = AppWindow::new().expect("Failed to load UI");
@@ -37,51 +175,25 @@ fn create_slint_app() -> AppWindow {
     ui
 }
 
-struct Esp32Platform {
-    window: Rc<MinimalSoftwareWindow>,
-    // optional: some timer device from your device's HAL crate
-    timer: hal::Timer,
-    // ... maybe more devices
-}
-
-impl Platform for Esp32Platform {
-    fn create_window_adapter(
-        &self,
-    ) -> Result<Rc<dyn slint::platform::WindowAdapter>, slint::PlatformError> {
-        // Since on MCUs, there can be only one window, just return a clone of self.window.
-        // We'll also use the same window in the event loop.
-        Ok(self.window.clone())
-    }
-    fn duration_since_start(&self) -> core::time::Duration {
-        core::time::Duration::from_micros(self.timer.get_time())
-    }
-    // optional: You can put the event loop there, or in the main function, see later
-    fn run_event_loop(&self) -> Result<(), slint::PlatformError> {
-        todo!();
-    }
-}
-
-
 fn main() {
     use esp_idf_svc::hal::sys::*;
 
     /* Initialize I2C (for touch and audio) */
-    unsafe { bsp_i2c_init(); }
+    unsafe {
+        bsp_i2c_init();
+    }
 
-    
     let mut io_handle: esp_lcd_panel_io_handle_t = std::ptr::null_mut();
     let mut panel_handle: esp_lcd_panel_handle_t = std::ptr::null_mut();
 
 
-    const DISPLAY_WIDTH: usize = 320;
-    const DISPLAY_HEIGHT: usize = 240;
-    const DRAW_BUFFER_SIZE: usize = DISPLAY_WIDTH * DISPLAY_HEIGHT; // @todo find constants
-
     let bsp_disp_cfg = bsp_display_config_t {
-        max_transfer_sz: (DRAW_BUFFER_SIZE * std::mem::size_of::<slint::platform::software_renderer::Rgb565Pixel>()) as i32,
+        max_transfer_sz: (DRAW_BUFFER_SIZE
+            * std::mem::size_of::<slint::platform::software_renderer::Rgb565Pixel>())
+            as i32,
     };
 
-    let mut touch_handle = std::ptr::null_mut();
+    let mut touch_handle: *mut esp_lcd_touch_s = std::ptr::null_mut();
     let bsp_touch_cfg = bsp_touch_config_t::default();
 
     unsafe {
@@ -89,7 +201,6 @@ fn main() {
         bsp_touch_new(&bsp_touch_cfg, &mut touch_handle);
         bsp_display_backlight_on();
     }
-    
 
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -98,57 +209,18 @@ fn main() {
 
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
-    
+
     // Configure platform for Slint
     let window = slint::platform::software_renderer::MinimalSoftwareWindow::new(Default::default());
 
-    slint::platform::set_platform(alloc::boxed::Box::new(Esp32Platform {
+
+    slint::platform::set_platform(alloc::boxed::Box::new(EspPlatform {
+        size: slint::PhysicalSize::new(DISPLAY_WIDTH as u32, DISPLAY_HEIGHT as u32),
+        panel_handle,
+        touch_handle: Some(touch_handle),
         window: window.clone(),
-        timer: hal::Timer(),
     }))
     .unwrap();
 
-
-    use slint::platform::software_renderer::Rgb565Pixel;
-    let mut buffer = [Rgb565Pixel(0); DISPLAY_WIDTH * DISPLAY_HEIGHT];
-
-    // ... configure the screen driver to use buffer1 or buffer2 ...
-
-    // ... rest of initialization ...
-
-    log::info!("Hello, world!");
-    loop {
-        // Let Slint run the timer hooks and update animations.
-        slint::platform::update_timers_and_animations();
-
-        // Check the touch screen or input device using your driver.
-        if let Some(event) = check_for_touch_event(/*...*/) {
-            // convert the event from the driver into a `slint::platform::WindowEvent`
-            // and pass it to the window.
-            window.dispatch_event(event);
-        }
-
-        // ... maybe some more application logic ...
-
-        // Draw the scene if something needs to be drawn.
-        window.draw_if_needed(|renderer| {
-
-            // Do the rendering!
-            renderer.render(&mut buffer, DISPLAY_WIDTH);
-
-            // tell the screen driver to display the other buffer.
-            swap_buffers();
-
-            // Swap the buffer references for our next iteration
-            // (this just swap the reference, not the actual data)
-        });
-
-        // Try to put the MCU to sleep
-        if !window.has_active_animations() {
-            if let Some(duration) = slint::platform::duration_until_next_timer_update() {
-                // ... schedule a timer interrupt in `duration` ...
-            }
-            hal::wfi(); // Wait for interrupt
-        }
-    }
+    create_slint_app().run().unwrap();
 }
