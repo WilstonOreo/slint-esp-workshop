4ebfae1d52677134b57db5a3ceb3346861442db9
Michael Winkelmann <michael@winkelmann.site>
2024-05-27 12:38:40 +0200

    Remove DHT read

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,17 @@
 mod dht22;
 mod esp32;
 
-use core::sync::atomic::Ordering;
-
 slint::include_modules!();
 
-static TEMPERATURE: portable_atomic::AtomicF32 = portable_atomic::AtomicF32::new(0.0);
-static HUMIDITY: portable_atomic::AtomicF32 = portable_atomic::AtomicF32::new(0.0);
-
 unsafe extern "C" fn dht_task(_: *mut core::ffi::c_void) {
-    //let dht = slint::platform::dht::Dht::new(4).unwrap();
     let dht = dht22::DHT22::new(13);
 
     loop {
         match dht.read() {
+            #[allow(unused_variables)]
             Ok((temperature, humidity)) => {
-                if temperature != TEMPERATURE.load(Ordering::Relaxed) || humidity != HUMIDITY.load(Ordering::Relaxed) {
-                    TEMPERATURE.store(temperature, Ordering::Relaxed);
-                    HUMIDITY.store(humidity, Ordering::Relaxed);
-                    log::info!("Temperature: {:.2}°C, Humidity: {:.2}%", temperature, humidity);
-                }
+                // TODO: Store sensor values somewhere
+                
             }
             Err(e) => {
                 log::error!("Error reading DHT22: {:?}", e);
@@ -59,13 +51,9 @@ fn main() {
     // Finally, run the app!
     let ui = AppWindow::new().expect("Failed to load UI");
 
-    let ui_handle = ui.as_weak();
     let timer = slint::Timer::default();
     timer.start(slint::TimerMode::Repeated, std::time::Duration::from_millis(2000), move || {
-        let ui = ui_handle.unwrap();
-
-        ui.set_temperature(TEMPERATURE.load(Ordering::Relaxed));
-        ui.set_humidity(HUMIDITY.load(Ordering::Relaxed));    
+        // TODO: Update UI with sensor values
     });
 
     ui.run().unwrap();
