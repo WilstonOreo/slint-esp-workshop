0d5d147e37bd8b5f66ac223ea5462911447642bb
Michael Winkelmann <michael@winkelmann.site>
2024-11-06 19:59:17 +0100

    Use correct slint file for wasm

--- wasm/build.rs
+++ wasm/build.rs
@@ -2,5 +2,5 @@ fn main() {
     // Compile the Slint file.
     // 
     // The appwindow.slint file is compiled into a Rust file that contains the UI code.
-    slint_build::compile("../ui/appwindow.slint").expect("Slint build failed");
+    slint_build::compile("appwindow.slint").expect("Slint build failed");
 }
