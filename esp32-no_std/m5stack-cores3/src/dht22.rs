b5d026e1755422de1379c73ce25fe3f8381ae821
Michael Winkelmann <michael@winkelmann.site>
2025-09-14 18:26:29 +0200

    Fix DHT22

--- esp32-no_std/m5stack-cores3/src/dht22.rs
+++ esp32-no_std/m5stack-cores3/src/dht22.rs
@@ -1,22 +1,13 @@
-use dht22_sensor::{Dht22, DhtError};
-use embedded_hal_bus::i2c::RefCellDevice;
-use esp_hal::gpio::{Flex, Pin};
+use dht22_sensor::DhtError;
+use esp_hal::gpio::Flex;
+
+use embassy_time::{Duration, Timer};
 
 #[embassy_executor::task]
-pub async fn dht22_task(mut dht_pin: esp_hal::gpio::Flex<'static>) {
-    let output_config = esp_hal::gpio::OutputConfig::default()
-        .with_drive_mode(esp_hal::gpio::DriveMode::OpenDrain)
-        .with_pull(esp_hal::gpio::Pull::None);
-    dht_pin.apply_output_config(&output_config);
-    dht_pin.set_input_enable(true);
-    dht_pin.set_output_enable(true);
-    dht_pin.set_level(esp_hal::gpio::Level::High);
-
+pub async fn dht22_task(mut dht_pin: Flex<'static>) {
     let mut delay = esp_hal::delay::Delay::new();
-    let delay1 = esp_hal::delay::Delay::new();
-    delay1.delay_millis(2000);
+    let mut sensor = dht22_sensor::Dht22::new(&mut dht_pin, &mut delay);
 
-    let mut sensor = Dht22::new(&mut dht_pin, &mut delay);
     loop {
         match sensor.read() {
             Ok(reading) => {
@@ -34,10 +25,11 @@ pub async fn dht22_task(mut dht_pin: esp_hal::gpio::Flex<'static>) {
                     log::info!("Timeout error");
                 }
                 DhtError::PinError(e) => {
-                    log::info!("Pin error:{}", e);
+                    log::info!("Pin error:{e}");
                 }
             },
         }
-        delay1.delay_millis(5000);
+
+        Timer::after(Duration::from_secs(5)).await;
     }
 }
